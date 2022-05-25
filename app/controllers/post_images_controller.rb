b46546c07c7d5d1e4a87dc1cf8704f10e8ee8a0f
EC2 Default User <ec2-user@ip-172-31-1-111.ec2.internal>
2022-05-25 14:38:27 +0000

    [update]Bookers2

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -19,7 +19,7 @@ class PostImagesController < ApplicationController
   end
 
   def show
-    @post_image =PostImage.find(params[:id])
+    @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
 
